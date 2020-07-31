07c6910afecd0e0cb228baadbcf4e386db6f1823
jinmountain <lvoemountain@gmail.com>
2020-07-31 17:06:24 -0500

    smashed youngest common ancestor

---
+++ algoExpert/youngestCommonAncestor/youngestCommonAncestor.py
@@ -0,0 +1,34 @@
+# This repository is only for the personal educational purpose 
+# and following code is from algoExpert
+
+class AncestralTree:
+    def __init__(self, name):
+        self.name = name
+        self.ancestor = None
+
+def getYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo):
+    depthOne = getDescendantDepth(descendantOne, topAncestor)
+    depthTwo = getDescendantDepth(descendantTwo, topAncestor)
+    if depthOne > depthTwo:
+    	return climbAncestry(descendantOne, descendantTwo, depthOne - depthTwo)
+    else:
+    	return climbAncestry(descendantTwo, descendantOne, depthTwo - depthOne)
+
+def getDescendantDepth(descendant, topAncestor):
+	depth = 0
+	while descendant != topAncestor:
+		depth += 1
+		descendant = descendant.ancestor
+	return depth
+
+def climbAncestry(lowerDescendant, higherDescendant, diff):
+	while diff > 0:
+		lowerDescendant = lowerDescendant.ancestor
+		diff -= 1
+	while lowerDescendant != higherDescendant:
+		lowerDescendant = lowerDescendant.ancestor
+		higherDescendant = higherDescendant.ancestor
+	return lowerDescendant
+
+
+
