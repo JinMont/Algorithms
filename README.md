16e5c1c024ea3ec291711db6adacb59b4b0eb412
jinmountain <lvoemountain@gmail.com>
2020-08-06 14:20:07 -0500

    added 'search in sorted matrix'

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ Name                       | Link
  Permutations Solution 1   |[Note](https://github.com/jinmountain/Algorithms/blob/master/algoExpert/permutations_sol1.py)
  Permutations Solution 2   |[Note](https://github.com/jinmountain/Algorithms/blob/master/algoExpert/permutations_sol2.py)
  Powerset                  |[Note](https://github.com/jinmountain/Algorithms/blob/master/algoExpert/powerset.py)
+ Search In Sorted Matrix   |[Note](https://github.com/jinmountain/Algorithms/blob/master/algoExpert/searchInSortedMatrix.py)
---
+++ algoExpert/searchInSortedMatrix.py
@@ -0,0 +1,23 @@
+# Search In Sorted Matrix
+
+# ==============
+# Before dive in
+# ==============
+# 1) I think it was similar to Binary tree in the idea that 
+#    you can increase row or decrease column based on the comparision of target and current postition
+# 2) Since the row and column are in sorted order, 
+#    you want to start at the top right corner to use the advantage moving row and column
+#  a) when you increase row => target > current position
+#  b) when you decrease column => target < current position
+
+def searchInSortedMatrix(matrix, target):
+	row = 0
+	col = len(matrix[0]) - 1
+	while row < len(matrix) and col >= 0:
+		if target == matrix[row][col]:
+			return [row, col]
+		elif target < matrix[row][col]:
+			col -= 1
+		else:
+			row += 1
+	return [-1, -1]
