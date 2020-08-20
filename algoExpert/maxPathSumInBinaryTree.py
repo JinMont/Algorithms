f378f028f2fea164e6dad61a64e20b14d1a10af4
jinmountain <lvoemountain@gmail.com>
2020-08-19 22:32:16 -0500

    added notes about max path sum

--- algoExpert/maxPathSumInBinaryTree.py
+++ algoExpert/maxPathSumInBinaryTree.py
@@ -1,3 +1,19 @@
+# MPS(T):
+#  LSB, LS = MPS(L)
+#  RSB, RS = MPS(R)
+#  MCSB = max(LSB, RSB) <= gives max of (Left Sum Branch and Right Sum Branch)
+#  MSB = max(MCSB + V, V) <= max of (MCSB + Current Value and Current Value)
+#  MST = max(MSB, LSB + V + RSB)
+#  RMPS = max(LS, RS, MRT)
+#  return (MSB, RMPS)
+
+# findMaxSum gives two values
+# 1) MSB => max value between its current value and its current value + max(left, right)
+# 2) RMPS => max of ( LS, RS, MST )
+#                      |
+#             (MSB, LSB + V + RSB)
+# 3) MSB is a max path of paths that can be used again
+# 4) RMPS is a max path of paths that can be used again or not
 
 def maxPathSum(tree):
     _, maxSum = findMaxSum(tree)
