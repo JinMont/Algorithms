bf0b924752f6ef63f6ac439864a9a4ad7f8eb67c
jinmountain <lvoemountain@gmail.com>
2020-08-28 17:58:16 -0500

    added 'knapsack problem'

---
+++ algoExpert/knapsackProblem.py
@@ -0,0 +1,14 @@
+#        0 1 2 3 4 5 6 7 8 9 10
+# [ ]    0 0 0 0 0 0 0 0 0 0 0
+# [1, 2] 0 0 1 1 1 1 1 1 1 1 1
+# [4, 3] 0 0 1 4 4 5 5 5 5 5 5
+# [5, 6] 0 0 1 4 4 5 5 5 6 9 9 
+# [6, 7] 0 0 1 4 4 5 5 6 6 7 10
+
+def knapsackProblem(items, capacity):
+    # Write your code here.
+    # return [
+    #   10, // total value
+    #   [1, 2], // item indices
+    # ]
+    pass
